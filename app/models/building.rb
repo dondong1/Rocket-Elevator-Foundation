9069ac195e31ae8da793b7bb26d4bcbb2ac43eba
Don <don_nguyen2001@yahoo.com>
2021-03-24 22:27:10 -0700

    Googmap done

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,11 +1,8 @@
 class Building < ApplicationRecord
-   has_many :building_details 
-   belongs_to :customer
-   belongs_to :address
-   has_many :batteries
-   has_many :interventions
+    belongs_to  :customer
+    has_many    :batteries
+    has_many    :building_details
+    belongs_to  :address
+    has_many :interventions
 
-   def loc_building
-    "#{address.address} - #{address.city}"
-   end
 end
