13d41cbd292b18ed97e3b3327699ed8c6bbd0f5f
Don <don_nguyen2001@yahoo.com>
2021-03-29 12:20:43 -0700

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,31 @@
 class ApplicationController < ActionController::Base
-          
+    protect_from_forgery prepend: true, with: exception      
     skip_before_action :verify_authenticity_token 
     
     
-    # Probably add more here to restrict users from directly accessing the back-office
-    # def require_admin # This is from Blazer..
-    #     # depending on your auth, something like...
-    #     redirect_to root_path unless current_user && current_user.superadmin?
+    # # Probably add more here to restrict users from directly accessing the back-office
+    # # def require_admin # This is from Blazer..
+    # #     # depending on your auth, something like...
+    # #     redirect_to root_path unless current_user && current_user.superadmin?
+    # # end
+
+    # protected
+    # #Jorge - redirecto to home after signin or signup
+    # def after_sign_in_path_for(resource)
+    #     '/index'
     # end
 
-    protected
-    #Jorge - redirecto to home after signin or signup
-    def after_sign_in_path_for(resource)
-        '/index'
-    end
+    # def after_sign_up_path_for(resource)
+    #     '/index' 
+    # end
 
-    def after_sign_up_path_for(resource)
-        '/index' 
-    end
-
-    def is_employee?
-        if !current_user || !current_user.employee
-            redirect_to '/index', alert: "You need to be a employee"
-        elsif  current_user and (current_user.superadmin_role? or current_user.employee) 
-        else
-            redirect_to '/index', alert: "You need to be a employee"
-        end
-    end
+    # def is_employee?
+    #     if !current_user || !current_user.employee
+    #         redirect_to '/index', alert: "You need to be a employee"
+    #     elsif  current_user and (current_user.superadmin_role? or current_user.employee) 
+    #     else
+    #         redirect_to '/index', alert: "You need to be a employee"
+    #     end
+    # end
 
 end
